8ce6c8ec3400953a5191554874e52a1c44b9296f
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-10-23 15:28:45 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "header.h"
 
+struct node{
+  int i;
+  struct node * next;
+}
+
+int main(){
+  printf("Printing Empty List:\n");
+  //struct node * start = insert_front(malloc(12), 
