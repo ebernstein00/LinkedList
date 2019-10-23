636a8eed490f753fa99eee725f5fd707ee459082
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-10-23 15:13:47 -0400

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,1 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+struct node{
+  int i;
+  struct node* next;
+};
+
+void print_list(struct node* node){
+  printf("Printing List:\n[ ");
+  while(node->next != NULL){
+    printf("%d ", node->i);
+  }
+}
+
+struct node * insert_front(struct node* node, int x){
+  struct node front;
+  front->i = x;
+  front->next = node;
+  struct node* ptr = &front;
+  return ptr;
+}
+
+struct node * free_list(struct node* node){
+  struct node* ph = node->next;
+  free(node);
+  free_list(ph);
+}
