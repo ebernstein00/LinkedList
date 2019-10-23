f9a1d04223d4f8393cf951c791cfe513ede472f6
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-10-23 17:55:53 -0400

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ struct node{
   struct node * next;
 };
 
-int main(){
+int mn(){
   printf("Printing Empty List:\n[  ]");
   //print adding stuff
   struct node * start = insert_front(malloc(sizeof(struct node)), 9);
