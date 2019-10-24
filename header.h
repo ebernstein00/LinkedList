45db98f1362b4af203cea140e3ca9cd246c04dc0
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-10-24 14:57:50 -0400

    Update header.h

--- header.h
+++ header.h
@@ -1,3 +1,4 @@
 void print_list(struct node* node);
 struct node* insert_front(struct node* node, int);
 struct node* free_list(struct node* node);
+struct node* remove(struct node *front, int data);
